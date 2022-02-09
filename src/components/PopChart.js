24dae9fae6a9146e816a53c9fd4a0f752d67667d
Rodney East <rodneyweast@gmail.com>
2022-02-09 17:43:23 -0600

    first commit

---
+++ src/components/PopChart.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class PopChart extends Component {
+  render() {
+      return (
+        <h1>Chart</h1>
+      )
+  }
+}
+
+export default PopChart;
